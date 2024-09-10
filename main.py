2a956cd144b05aa62418cccaff6c23ec6558c610
Aleksei Tishkov <tscenic.ua@gmail.com>
2024-09-10 14:26:15 +0300

    script partly rewritten to meet new requirements; only 2 reports are used to form all 8 needed; a couple of new functions implemented; html email content applied

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from datetime import date
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
 from email import encoders
 import pandas as pd
+from io import StringIO
 
 import settings
 
@@ -13,79 +15,84 @@ import settings
 today = date.today()
 
 if today.weekday() == 0:
-    report_web_file_solta_url = settings.report_web_file_solta_weekends_url
-    report_app_file_solta_url = settings.report_app_file_solta_weekends_url
-    report_web_file_other_url = settings.report_web_file_other_weekends_url
-    report_app_file_other_url = settings.report_web_file_other_weekends_url
+    report_web_file_url = settings.report_web_file_weekends_url
+    report_app_file_url = settings.report_app_file_weekends_url
 else:
-    report_web_file_solta_url = settings.report_web_file_solta_weekdays_url
-    report_app_file_solta_url = settings.report_app_file_solta_weekdays_url
-    report_web_file_other_url = settings.report_web_file_other_weekdays_url
-    report_app_file_other_url = settings.report_web_file_other_weekdays_url
+    report_web_file_url = settings.report_web_file_weekdays_url
+    report_app_file_url = settings.report_app_file_weekdays_url
 
 today = today.strftime("%d.%m.%Y")
 
-other_web_file_name = f'other_web_moderation_{today}.csv'
-other_web_file_path = f'{settings.file_path}/{today}/{other_web_file_name}'
-other_app_file_name = f'other_app_moderation_{today}.csv'
-other_app_file_path = f'{settings.file_path}/{today}/{other_app_file_name}'
-
-solta_web_file_name = f'solta_web_moderation_{today}.csv'
-solta_web_file_path = f'{settings.file_path}/{today}/{other_web_file_name}'
-solta_app_file_name = f'solta_app_moderation_{today}.csv'
-solta_app_file_path = f'{settings.file_path}/{today}/{other_app_file_name}'
-
-email_subject = f'{settings.email_subject} {today}'
+email_subject = f'{settings.email_subject}{today}'
 
 file_path = f'{settings.file_path}//{today}'
 
+message_high_text = settings.message_high_text
+message_low_text = settings.message_low_text
 
-def process_and_attach(message, path, response, name):
-    with open(path, 'wb') as file:
-        file.write(response.content)
 
-    attachment = MIMEBase('application', 'octet-stream')
-
-    with open(path, 'rb') as file:
-        df = pd.read_csv(file)
-        if len(df.index):  # Условие проверки на не-пустой файл
-            file.seek(0)
-            attachment.set_payload(file.read())
-            print(df.head())
-            print(f'Файл {path} успешно создан')
+def check_and_attach(message, d):
+    for filename, dataframe in d.items():
+        if sum(dataframe.index) > 1:
+            attachment = MIMEBase('application', 'octet-stream')
+            dataframe.to_csv(f'{file_path}//{filename}_{today}.csv', index=False)
+            csv_data = dataframe.to_csv(index=False)
+            attachment.set_payload(csv_data.encode('utf-8'))
             encoders.encode_base64(attachment)
-            attachment.add_header('Content-Disposition', f'attachment; filename={name}')
+            attachment.add_header('Content-Disposition', f'attachment; filename="{filename}_{today}.csv"')
             message.attach(attachment)
         else:
-            print(f'C файлом {path} что-то не так, проверьте')
+            print(f'C файлом {filename} что-то не так, проверьте')
+
+
+def process_and_attach(message_high, message_low, path, tp):
+    for _ in range(3):
+        response = requests.get(path)
+        if response.status_code == 200:
+            break
+    else:
+        raise BaseException('Ошибка доступа к серверу с отчетами')
+
+    df = pd.read_csv(StringIO(response.text))
+
+    df_solta = df[df['dsp'] == 'solta'].iloc[:, 1:]
+    df_other = df[df['dsp'] == 'other'].iloc[:, 1:]
+
+    df_other_high = df_other[df_other['count'] >= 100]
+    df_other_low = df_other[df_other['count'] < 100]
+
+    df_solta_high = df_solta[df_solta['count'] >= 25]
+    df_solta_low = df_solta[df_solta['count'] < 25]
+
+    check_and_attach(message_high, {f'solta_{tp}_high': df_solta_high, f'other_{tp}_high': df_other_high})
+    check_and_attach(message_low, {f'solta_{tp}_low': df_solta_low, f'other_{tp}_low': df_other_low})
 
 
 def main():
     os.mkdir(f'{settings.file_path}\\{today}')
 
-    solta_web_response = requests.get(report_web_file_solta_url)
-    solta_app_response = requests.get(report_app_file_solta_url)
+    message_high = MIMEMultipart()
+    message_high['From'] = settings.sender_email
+    message_high['To'] = settings.receiver_email
+    message_high['Subject'] = f'{email_subject}. Высокий приоритет'
 
-    other_web_response = requests.get(report_web_file_other_url)
-    other_app_response = requests.get(report_app_file_other_url)
+    message_low = MIMEMultipart()
+    message_low['From'] = settings.sender_email
+    message_low['To'] = settings.receiver_email
+    message_low['Subject'] = f'{email_subject}. Низкий приоритет'
 
-    message = MIMEMultipart()
-    message['From'] = settings.sender_email
-    message['To'] = settings.receiver_email
-    message['Subject'] = email_subject
+    message_high.attach(MIMEText(message_high_text, 'html'))
+    message_low.attach(MIMEText(message_low_text, 'html'))
 
-    # process_and_attach(message, solta_app_file_path, solta_app_response, solta_app_file_name)
-    # process_and_attach(message, solta_web_file_path, solta_web_response, solta_web_file_name)
-
-    process_and_attach(message, other_app_file_path, other_app_response, other_app_file_name)
-    process_and_attach(message, other_web_file_path, other_web_response, other_web_file_name)
+    process_and_attach(message_high, message_low, report_web_file_url, 'web')
+    process_and_attach(message_high, message_low, report_app_file_url, 'inapp')
 
     with smtplib.SMTP(settings.smtp_server, 587) as server:
         server.starttls()
         server.login(settings.sender_email, settings.sender_password)
-        server.sendmail(settings.sender_email, settings.receiver_email, message.as_string())
-        print(f'Email "{email_subject}" отправлен')
-
+        server.sendmail(settings.sender_email, settings.receiver_email, message_high.as_string())
+        server.sendmail(settings.sender_email, settings.receiver_email, message_low.as_string())
+        print(f'Email-ы за {today} отправлены')
     print('Нажмите что-то для завершения работы скрипта')
     input()
 
--- settings.py
+++ settings.py
@@ -11,14 +11,13 @@ sender_password = os.getenv('GMAIL_APP_CODE')
 smtp_server = 'smtp.gmail.com'
 email_subject = os.getenv('EMAIL_SUBJECT')
 
-report_app_file_solta_weekdays_url = os.getenv('REPORT_FILE_INAPP_SOLTA_WEEKDAYS')
-report_web_file_solta_weekdays_url = os.getenv('REPORT_FILE_WEB_SOLTA_WEEKDAYS')
-report_app_file_solta_weekends_url = os.getenv('REPORT_FILE_INAPP_SOLTA_WEEKENDS')
-report_web_file_solta_weekends_url = os.getenv('REPORT_FILE_WEB_SOLTA_WEEKENDS')
+report_app_file_weekends_url = os.getenv('REPORT_FILE_INAPP_WEEKENDS')
+report_web_file_weekends_url = os.getenv('REPORT_FILE_WEB_WEEKENDS')
 
-report_app_file_other_weekdays_url = os.getenv('REPORT_FILE_INAPP_OTHER_WEEKDAYS')
-report_web_file_other_weekdays_url = os.getenv('REPORT_FILE_WEB_OTHER_WEEKDAYS')
-report_app_file_other_weekends_url = os.getenv('REPORT_FILE_INAPP_OTHER_WEEKENDS')
-report_web_file_other_weekends_url = os.getenv('REPORT_FILE_WEB_OTHER_WEEKENDS')
+report_web_file_weekdays_url = os.getenv('REPORT_FILE_WEB_WEEKDAYS')
+report_app_file_weekdays_url = os.getenv('REPORT_FILE_INAPP_WEEKDAYS')
 
 file_path = os.getenv('FILE_PATH')
+
+message_high_text = os.getenv('MESSAGE_HIGH_TEXT')
+message_low_text = os.getenv('MESSAGE_LOW_TEXT')
